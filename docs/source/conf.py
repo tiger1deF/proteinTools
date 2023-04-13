b19a42f952ecd91f2b72cf61612641aae80bdab7
Christian de Frondeville <defrondeville.c@northeastern.edu>
2023-04-13 11:24:53 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../proteinTools'))
+sys.path.insert(0, os.path.abspath('../../proteinTools'))
 
 project = 'proteinTools'
 copyright = '2023, Christian de Frondeville'
@@ -24,7 +24,7 @@ extensions = [
    'sphinx.ext.autodoc',
    'sphinx.ext.autosummary',
 ]
-
+autodoc_mock_imports = ["pandas", "flask", "urllib3", "mygene", "chembl-webresource-client"]
 # -- Project information
 
 templates_path = ['_templates']
