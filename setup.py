6d43708686857561d81c0f079b6826d6d2d2c395
Christian de Frondeville <defrondeville.c@northeastern.edu>
2023-09-02 00:52:34 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -6,11 +6,10 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name = "proteinTools",
-    version = "0.1.8",
+    version = "1.9.1",
     author = "Christian de Frondeville",
-    description = "Lightweight package which simplifies interacting with proteins.",
+    description = "Lightweight, object-oriented backend bioinformatics package which simplifies interacting with proteins.",
     long_description = long_description,
     packages = ["proteinTools"],
-    install_requires=['pandas','urllib3','chembl-webresource-client','mygene']
-
+    install_requires=['pandas','urllib3','mygene', 'pubchempy', 'stringdb'] #'chembl-webresource-client',
 )
